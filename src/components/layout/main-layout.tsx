dfcbac982480bb381ef2e72c7fe04913351ea72a
developerfabiocsilva@outlook.com <developerfabiocsilva@outlook.com>
2025-09-28 23:24:58 -0300

    feat: Create components main-layout.tsx

---
+++ src/components/layout/main-layout.tsx
@@ -0,0 +1,34 @@
+
+'use client';
+
+import { useState } from 'react';
+import { Header } from './header';
+import { Sidebar } from './sidebar';
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export function MainLayout({ children }: MainLayoutProps) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header onMenuClick={() => setSidebarOpen(true)} />
+      
+      <div className="flex">
+        <Sidebar 
+          isOpen={sidebarOpen} 
+          onClose={() => setSidebarOpen(false)} 
+        />
+        
+        {/* Main content */}
+        <main className="flex-1 lg:ml-0">
+          <div className="p-4 sm:p-6 lg:p-8">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
